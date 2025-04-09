29ba87852940b7a5e64f6b0e87aab47a170ba87c
Fabio Nicolau <ophiuchus85@gmail.com>
2025-04-09 03:30:53 -0300

    Fav Icons and Coming soon

--- src/resources/views/coinllery/welcome.blade.php
+++ src/resources/views/coinllery/welcome.blade.php
@@ -2,14 +2,50 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="ALL" />
+        <meta name="author" content="Qab and Ophiuchus" />
 
-        <title>Coinllery</title>
+        <meta name="description" content="Coinllery is a Gallery made for Zora Coins" />
+        <meta name="keywords" content="Coinllery, Zora, Coin, SDK, Comunity, Gallery" />
+        <meta name="copyright" content="&copy; 2025 Coinllery&reg; by Qab and Ophiuchus. All Rights Reserved" />
+
+        <meta name="screen-orientation" content="portrait">
+
+        <title>Coinllery – Coming Soon</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+        <!-- Icons -->
+        <link rel="apple-touch-icon" sizes="57x57" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/favicon-16x16.png">
+        
+        <link rel="manifest" href="/manifest.json">
+        <link rel="shortcut icon" href="https://coinllery.io/favicon.ico">
+        <link rel="icon" type="image/png" sizes="32x32" href="https://coinllery.io/favicon.png">
+        <link rel="icon" type="image/gif" sizes="32x32" href="https://coinllery.io/favicon.gif">
+
+        <!-- Windows  -->
+        <meta name="msapplication-navbutton-color" content="white">
+        <meta name="msapplication-TileColor" content="white">
+        <meta name="msapplication-TileImage" content="https://storage.googleapis.com/coinllery-app-storage/app/media/icons/apple-icon-144x144.png">
+        <meta name="msapplication-config" content="/browserconfig.xml">
 
         <style>
 
@@ -115,7 +151,7 @@
             <div class="centralized-box moving-gradient">
                 <img src="https://storage.googleapis.com/coinllery-app-storage/app/media/images/coinllery_logo.png" alt="Coinllery">
             </div>
-            <h1 class="text-center text-uppercase colorful text-light p-10">Comming Soon</h1>
+            <h1 class="text-center text-uppercase colorful text-light p-10">Coming Soon</h1>
         </section>
         <footer>
             <p>Created with 💜 by <a href="https://zora.co/@qabqabqab" target="_blank"><strong>@qabqabqab</strong></a> and <a href="https://zora.co/@ophiuchus85" target="_blank"><strong>@ophiuchus85</strong></a></p>
