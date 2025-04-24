97e93a35238beacf7560f07b4f75ab7ea3e3bfba
Fabio Nicolau <ophiuchus85@gmail.com>
2025-04-23 22:59:00 -0300

    Frontend Implementation

---
+++ src/resources/views/coinllery/coinllery.blade.php
@@ -0,0 +1,69 @@
+@extends('coinllery.layouts.default')
+
+@section('content')
+    <section class="container justify-content-center">
+        <article class="col">
+            <figure class="colorful">
+                <img src="https://media.decentralized-content.com/-/rs:fill:2000:2000/g:ce/f:webp/aHR0cHM6Ly9tYWdpYy5kZWNlbnRyYWxpemVkLWNvbnRlbnQuY29tL2lwZnMvYmFmeWJlaWV4dDdpN2g1emR2YW1scHdhNGhudmdvenhpN2toYmJlYzdlYmhpZHE0MzN0YWZkMnhsYm0" alt="Coinllery Image">
+            </figure>
+            
+            <footer>
+                <p><strong>Coinllery by Qab</strong></p>
+                <p>Some representative placeholder content for the coinllery made by Qab.</p>
+            </footer>
+        </article>
+    </section>
+    <section class="container justify-content-center pt-3">
+        <article class="col">
+            <figure class="colorful">
+                <img src="https://media.decentralized-content.com/-/rs:fill:2000:2000/g:ce/f:webp/aHR0cHM6Ly9tYWdpYy5kZWNlbnRyYWxpemVkLWNvbnRlbnQuY29tL2lwZnMvYmFmeWJlaWV4dDdpN2g1emR2YW1scHdhNGhudmdvenhpN2toYmJlYzdlYmhpZHE0MzN0YWZkMnhsYm0" alt="Coinllery Image">
+            </figure>
+            
+            <footer class="container">
+                <div class="row">
+                    <div class="col">
+                        <img class="coin-author-thumb" src="https://media.decentralized-content.com/-/rs:fill:2000:2000/g:ce/f:webp/aHR0cHM6Ly9tYWdpYy5kZWNlbnRyYWxpemVkLWNvbnRlbnQuY29tL2lwZnMvYmFmeWJlaWV4dDdpN2g1emR2YW1scHdhNGhudmdvenhpN2toYmJlYzdlYmhpZHE0MzN0YWZkMnhsYm0" alt="">
+                        authorname
+                    </div>
+                    <div class="col d-none">
+                        <div class="row justify-content-end">
+                            <div class="col-auto">6h</div>
+                            <div class="col-auto">Share</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row pt-4">
+                    <div class="col">
+                        <p><strong>Coin Name</strong></p>
+                        <p>Coin Description</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col text-center">
+                        <table class="table coin-stats">
+                            <thead>
+                                <tr>
+                                    <th scope="col"><span>Market Cap</span></th>
+                                    <th scope="col"><span>24H Volume</span></th>
+                                    <th scope="col"><span>Creator Earning</span></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>$1.403</td>
+                                    <td>$0</td>
+                                    <td>$0</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col text-center">
+                        <a href="#" class="btn btn-buy">Buy</a>
+                    </div>
+                </div>
+            </footer>
+        </article>
+    </section>
+@endsection
